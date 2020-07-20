1824ee4a996d78f69245e084e3c5601365c381c1
MyosotisM2U <jeffreydebate@gmail.com>
2020-07-20 16:15:05 -0500

    test quickstart.py works

--- quickstart.py
+++ quickstart.py
@@ -29,7 +29,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'jeffrey_credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
